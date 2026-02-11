e47a6bee013fda7b193f1fdeae653f3383f3d83b
DevExpressExampleBot <scmaintenance@devexpress.com>
2026-02-11 15:09:56 +0400

    Source auto update [skip ci]

--- VB/DetermineContentType/MainWindow.xaml.vb
+++ VB/DetermineContentType/MainWindow.xaml.vb
@@ -1,38 +1,33 @@
-﻿Imports DevExpress.Pdf
+Imports DevExpress.Pdf
 Imports DevExpress.Xpf.PdfViewer
 Imports System.Windows
 Imports System.Windows.Input
 
 Namespace DetermineContentType
 
-    Partial Public Class MainWindow
+    Public Partial Class MainWindow
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
-            viewer.OpenDocument("..\..\demo.pdf")
+            Me.InitializeComponent()
+            Me.viewer.OpenDocument("..\..\demo.pdf")
         End Sub
 
         Private Sub viewer_MouseMove(ByVal sender As Object, ByVal e As MouseEventArgs)
-
-            Dim result As PdfHitTestResult = viewer.HitTest(e.GetPosition(viewer))
+            Dim result As PdfHitTestResult = Me.viewer.HitTest(e.GetPosition(Me.viewer))
             Dim contentTypeText As String = If(result.IsSelected, "Selected ", "Unselected ")
-
             Select Case result.ContentType
                 Case PdfDocumentContentType.Text
-                contentTypeText = contentTypeText & "Text"
+                    contentTypeText = contentTypeText & "Text"
                 Case PdfDocumentContentType.Image
-                contentTypeText = contentTypeText & " Image"
+                    contentTypeText = contentTypeText & " Image"
                 Case PdfDocumentContentType.Annotation
-                contentTypeText = contentTypeText & "Annotation"
+                    contentTypeText = contentTypeText & "Annotation"
                 Case Else
-                contentTypeText = "The content is empty"
+                    contentTypeText = "The content is empty"
             End Select
-            barButtonItem.Content = contentTypeText
+
+            Me.barButtonItem.Content = contentTypeText
         End Sub
     End Class
 End Namespace
-
-
-
-
